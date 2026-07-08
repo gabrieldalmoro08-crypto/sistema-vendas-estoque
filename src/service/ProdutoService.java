003adfc72470a5a3a18be158c21503401b57ac16
Gabriel <gabrieldalmoro08@gmail.com>
2026-07-08 20:45:28 -0300

    refactor: otimiza ProdutoService e inicia estrutura do Venda Service

--- src/service/ProdutoService.java
+++ src/service/ProdutoService.java
@@ -5,6 +5,8 @@ import service.*;
 import dao.*;
 import controller.*;
 
+import java.util.List;
+
 public class ProdutoService {
 
     private ProdutoDAO produtoDAO;
@@ -15,17 +17,12 @@ public class ProdutoService {
 
     public void cadastroProdutos(Produto produto){
 
-        if(produto.getPreco() <= 0){
-            throw new IllegalArgumentException("Erro: O preço do produto deve ser maior que zero!");
-        }
+        validarDadosBasicosProduto(produto);
 
         if(produto.getQtde() <= 0){
             throw new IllegalArgumentException("Erro: A quantidade inicial em estoque não pode ser negativa!");
         }
 
-        if(produto.getDescricao() == null || produto.getDescricao().trim().isEmpty()){
-            throw new IllegalArgumentException("Erro: O nome do produto não pode ficar em branco!");
-        }
         produtoDAO.cadastrarProduto(produto);
     }
 
@@ -42,6 +39,48 @@ public class ProdutoService {
         }
 
         produtoDAO.excluirProduto(produto.getId());
+    }
+
+    public void atualizacaoProduto(Produto produto){
+
+        if(produto == null || produto.getId() <= 0){
+            throw new IllegalArgumentException("Erro: Produto inválido ou não selecionado para edição!");
+        }
+
+        validarDadosBasicosProduto(produto);
+
+        if(produto.getQtde() < 0){
+            throw new IllegalArgumentException("Erro: Quantidade em estoque não pode ser negativa!");
+        }
+
+        produtoDAO.atualizarProduto(produto);
+
+    }
+
+    private void validarDadosBasicosProduto(Produto produto) {
+        if (produto == null) {
+            throw new IllegalArgumentException("Erro: Os dados do produto estão vazios!");
+        }
+
+        if (produto.getPreco() <= 0) {
+            throw new IllegalArgumentException("Erro: O preço do produto deve ser maior que zero!");
+        }
+
+        if (produto.getDescricao() == null || produto.getDescricao().trim().isEmpty()) {
+            throw new IllegalArgumentException("Erro: O nome do produto não pode ficar em branco!");
+        }
+    }
+
+    public Produto buscarPorProdutoId(int id){
+
+        if(id <= 0){
+            throw new IllegalArgumentException("Erro: Id inválido para busca!");
+        }
+        return produtoDAO.buscarProdutoPorId(id);
+    }
+
+    public List<Produto> listarProdutos(){
+        return produtoDAO.listarTodosProdutos();
     }
 
 }
--- src/service/VendaService.java
+++ src/service/VendaService.java
@@ -1,4 +1,18 @@
 package service;
 
+import dao.*;
+import model.*;
+
 public class VendaService {
+
+    private VendaDAO vendaDAO;
+
+    public VendaService(VendaDAO DAOvenda) {
+        this.vendaDAO = DAOvenda;
+    }
+
+    public void realizarVenda(Venda venda){
+
+
+    }
 }
